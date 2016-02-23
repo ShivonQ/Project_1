5260a95d1ec6a8a42cc88422afc772a4f621efeb
ShivonQ <realitykeeper@gmail.com>
2016-02-22 22:11:21 -0600

    Map Page:    No Change Until File Hosting    TODO: Add the form for
    Calendar Page:
      Date information is now working perfectly. Utilized local storage since with 2 windows the sessionStorage become different immediately after generation.
    
      NEXT: Make a new Branch for getting the lists of event data to display for each day.
    
     Main Page:
    
     Date Class + Data
       I have added lots of information on Ponj, will add more in time.
    
     City Class + Data
       Just beginning to define what the class will have.  Trying to get a image to be in the class for my city maps.

--- javascript_files/settlement_class.js
+++ javascript_files/settlement_class.js
@@ -4,8 +4,22 @@
 var list_of_settlement_types={1:"Thorp",2:"Hamlet",3:"Village",4:"Small Town",5:"Large Town",6:"Small City",7:"Large City",8:"Metropolis"}
 //since at the moment this class CANNOT take information since my whole world is already hardcoded I will just be doing the selection of capital cities and towns
 var Settlement_Data=function(){
+    this.name="";
+    this.country_or_area="";
 
-}
+    this.population=0;
+    this.money_limit=0;
+
+    this.power_center_in_community="";
+    this.power_center_alignement="";
+    this.authority_figures=[];
+
+    this.guardsmen=0;
+    this.major_exports=[];
+    this.major_imports=[];
+    //TODO figure out how to tie an image here. Stick it in a pannable box.
+    this.city_map=null;
+};
 
 
 
