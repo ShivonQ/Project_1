0548e503df870822e0bab4c62cd61638e76b468f
ShivonQ <realitykeeper@gmail.com>
2016-02-22 20:36:29 -0600

    Map Page:    No Change Until File Hosting
    Calendar Page:
      Date information is now working perfectly. Utilized local storage since wit 2 windows the storages become different immediately.
    
      NEXT: Make a new Branch for getting the lists of event data to display for each day.
    
     Main Page:
    
     Date Class + Data
       I have added lots of information on Ponj.

--- javascript_files/day_class.js
+++ javascript_files/day_class.js
@@ -50,123 +50,123 @@ Date_Information.prototype.Add_Information=function(string_of_input){
 };
 //No Precip, No Storm, so not declared
 
-var ponj1=new  Date_Information(1,"Ponj");
-var ponj2 = new Date_Information(2,"Ponj");
-var ponj3 = new Date_Information(3,"Ponj");
-var ponj4 = new Date_Information(4,"Ponj");
-var ponj5 = new Date_Information(5,"Ponj");
-var ponj6 = new Date_Information(6,"Ponj");
-var ponj7 = new Date_Information(7,"Ponj");
-var ponj8 = new Date_Information(8,"Ponj");
-var ponj9 = new Date_Information(9,"Ponj");
-var ponj10 = new Date_Information(10,"Ponj");
-var ponj11 = new Date_Information(11,"Ponj");
-var ponj12 = new Date_Information(12,"Ponj");
-var ponj13 = new Date_Information(13,"Ponj");
-var ponj14 = new Date_Information(14,"Ponj");
-var ponj15 = new Date_Information(15,"Ponj");
-var ponj16 = new Date_Information(16,"Ponj");
-var ponj17 = new Date_Information(17,"Ponj");
-var ponj18 = new Date_Information(18,"Ponj");
-var ponj19 = new Date_Information(19,"Ponj");
-var ponj20 = new Date_Information(20,"Ponj");
-var ponj21 = new Date_Information(21,"Ponj");
-var ponj22 = new Date_Information(22,"Ponj");
-var ponj23 = new Date_Information(23,"Ponj");
-var ponj24 = new Date_Information(24,"Ponj");
-var ponj25 = new Date_Information(25,"Ponj");
-var ponj26 = new Date_Information(26,"Ponj");
-var ponj27 = new Date_Information(27,"Ponj");
-var month_of_ponj={1:ponj1,2:ponj2,3:ponj3,4:ponj4,5:ponj5,6:ponj6,7:ponj7,8:ponj8,9:ponj9,10:ponj10,
-    11:ponj11, 12:ponj12,13:ponj13,14:ponj14,15:ponj15,16:ponj16,17:ponj17,18:ponj18,19:ponj19,
-    20:ponj20,21:ponj21, 22:ponj22, 23:ponj23,24:ponj24, 25:ponj25,26:ponj26,27:ponj27};
+var Ponj1=new  Date_Information(1,"Ponj");
+var Ponj2 = new Date_Information(2,"Ponj");
+var Ponj3 = new Date_Information(3,"Ponj");
+var Ponj4 = new Date_Information(4,"Ponj");
+var Ponj5 = new Date_Information(5,"Ponj");
+var Ponj6 = new Date_Information(6,"Ponj");
+var Ponj7 = new Date_Information(7,"Ponj");
+var Ponj8 = new Date_Information(8,"Ponj");
+var Ponj9 = new Date_Information(9,"Ponj");
+var Ponj10 = new Date_Information(10,"Ponj");
+var Ponj11 = new Date_Information(11,"Ponj");
+var Ponj12 = new Date_Information(12,"Ponj");
+var Ponj13 = new Date_Information(13,"Ponj");
+var Ponj14 = new Date_Information(14,"Ponj");
+var Ponj15 = new Date_Information(15,"Ponj");
+var Ponj16 = new Date_Information(16,"Ponj");
+var Ponj17 = new Date_Information(17,"Ponj");
+var Ponj18 = new Date_Information(18,"Ponj");
+var Ponj19 = new Date_Information(19,"Ponj");
+var Ponj20 = new Date_Information(20,"Ponj");
+var Ponj21 = new Date_Information(21,"Ponj");
+var Ponj22 = new Date_Information(22,"Ponj");
+var Ponj23 = new Date_Information(23,"Ponj");
+var Ponj24 = new Date_Information(24,"Ponj");
+var Ponj25 = new Date_Information(25,"Ponj");
+var Ponj26 = new Date_Information(26,"Ponj");
+var Ponj27 = new Date_Information(27,"Ponj");
+var month_of_ponj={1:Ponj1,2:Ponj2,3:Ponj3,4:Ponj4,5:Ponj5,6:Ponj6,7:Ponj7,8:Ponj8,9:Ponj9,10:Ponj10,
+    11:Ponj11, 12:Ponj12,13:Ponj13,14:Ponj14,15:Ponj15,16:Ponj16,17:Ponj17,18:Ponj18,19:Ponj19,
+    20:Ponj20,21:Ponj21, 22:Ponj22, 23:Ponj23,24:Ponj24, 25:Ponj25,26:Ponj26,27:Ponj27};
 
-ponj1.high_temprature=57.5;
-ponj1.low_temprature=42.4;
-ponj1.windspeed=4;
-ponj1.wind_direction="South East";
-ponj1.simple_information_to_display=["Bazz the Kobold Arrives in Weston with Arayan RoughStepper. Gets room at The Oldest Stone Building."];
+Ponj1.high_temprature=57.5;
+Ponj1.low_temprature=42.4;
+Ponj1.windspeed=4;
+Ponj1.wind_direction="South East";
+Ponj1.simple_information_to_display=["Bazz the Kobold Arrives in Weston with Arayan RoughStepper. Gets room at The Oldest Stone Building."];
 //
-ponj2.high_temprature=50.5;
-ponj2.low_temprature=38.4;
-ponj2.windspeed=2;
-ponj2.wind_direction="South";
-ponj2.simple_information_to_display=["Bazz gets a temp job in the mine."];
+Ponj2.high_temprature=50.5;
+Ponj2.low_temprature=38.4;
+Ponj2.windspeed=2;
+Ponj2.wind_direction="South";
+Ponj2.simple_information_to_display=["Bazz gets a temp job in the mine."];
 
-ponj3.high_temprature=48.0;
-ponj3.low_temprature=40.0;
-ponj3.windspeed=4;
-ponj3.precipitation=7;
-ponj3.precipitation_type="Rain";
-ponj3.precipitation_duration=2;
-ponj3.wind_direction="South East";
-ponj3.simple_information_to_display=["Nothing of Note"];
+Ponj3.high_temprature=48.0;
+Ponj3.low_temprature=40.0;
+Ponj3.windspeed=4;
+Ponj3.precipitation=7;
+Ponj3.precipitation_type="Rain";
+Ponj3.precipitation_duration=2;
+Ponj3.wind_direction="South East";
+Ponj3.simple_information_to_display=["Nothing of Note"];
 
-ponj4.high_temprature=52;
-ponj4.low_temprature=43;
-ponj4.windspeed=1;
-ponj4.wind_direction="East";
-ponj4.simple_information_to_display=["Nothing of Note"];
+Ponj4.high_temprature=52;
+Ponj4.low_temprature=43;
+Ponj4.windspeed=1;
+Ponj4.wind_direction="East";
+Ponj4.simple_information_to_display=["Nothing of Note"];
 
-ponj5.high_temprature=43.0;
-ponj5.low_temprature=32.0;
-ponj5.windspeed=4;
-ponj5.wind_direction="South East";
-ponj5.simple_information_to_display=["Nothing of Note"];
+Ponj5.high_temprature=43.0;
+Ponj5.low_temprature=32.0;
+Ponj5.windspeed=4;
+Ponj5.wind_direction="South East";
+Ponj5.simple_information_to_display=["Nothing of Note"];
 
-ponj6.high_temprature=48.0;
-ponj6.low_temprature=40.0;
-ponj6.windspeed=4;
-ponj6.precipitation=7;
-ponj6.precipitation_type="Rain";
-ponj6.precipitation_duration=2;
-ponj6.wind_direction="South East";
-ponj6.simple_information_to_display=["Nothing of Note"];
+Ponj6.high_temprature=48.0;
+Ponj6.low_temprature=40.0;
+Ponj6.windspeed=4;
+Ponj6.precipitation=7;
+Ponj6.precipitation_type="Rain";
+Ponj6.precipitation_duration=2;
+Ponj6.wind_direction="South East";
+Ponj6.simple_information_to_display=["Nothing of Note"];
 
-ponj7.high_temprature=41.0;
-ponj7.low_temprature=23.0;
-ponj7.windspeed=3;
-ponj7.precipitation=7;
-ponj7.precipitation_type="Freezing Rain";
-ponj7.precipitation_duration=5;
-ponj7.is_a_storm=true;
-ponj7.wind_direction="South East";
-ponj7.simple_information_to_display=["First Sleet Storm of Fall at 7pm.","Reports of growing numbers of crows in the fields from outlying farmers."];
+Ponj7.high_temprature=41.0;
+Ponj7.low_temprature=23.0;
+Ponj7.windspeed=3;
+Ponj7.precipitation=7;
+Ponj7.precipitation_type="Freezing Rain";
+Ponj7.precipitation_duration=5;
+Ponj7.is_a_storm=true;
+Ponj7.wind_direction="South East";
+Ponj7.simple_information_to_display=["First Sleet Storm of Fall at 7pm.","Reports of growing numbers of crows in the fields from outlying farmers."];
 
-ponj8.high_temprature=53.0;
-ponj8.low_temprature=38.0;
-ponj8.windspeed=5;
-ponj8.wind_direction="South";
-ponj8.simple_information_to_display=["Nothing of Note"];
+Ponj8.high_temprature=53.0;
+Ponj8.low_temprature=38.0;
+Ponj8.windspeed=5;
+Ponj8.wind_direction="South";
+Ponj8.simple_information_to_display=["Nothing of Note"];
 
-ponj9.high_temprature=45.0;
-ponj9.low_temprature=38.0;
-ponj9.windspeed=0;
-ponj9.simple_information_to_display=["Guardsman Beece stays late at the tavern, complaining of bad dreams. Offers to draw things for people."];
+Ponj9.high_temprature=45.0;
+Ponj9.low_temprature=38.0;
+Ponj9.windspeed=0;
+Ponj9.simple_information_to_display=["Guardsman Beece stays late at the tavern, complaining of bad dreams. Offers to draw things for people."];
 
-ponj10.high_temprature=44.0;
-ponj10.low_temprature=37.0;
-ponj10.windspeed=0;
-ponj10.simple_information_to_display=[
+Ponj10.high_temprature=44.0;
+Ponj10.low_temprature=37.0;
+Ponj10.windspeed=0;
+Ponj10.simple_information_to_display=[
     "Dumavil Cehris Gladdenstone arrives in town from Fallcrest, with Roondar her giant red panda companion.",
     "Cehris, Bazz, Seymour, and Kelvin meet eachother for real.",
     "Sithril Seranth the bard introduces herself.",
     "Guardsman Beece has a fit of some kind when Seymour confronts him about his freaky art, and runs out of the tavern."];
 
-ponj11.high_temprature=48.0;
-ponj11.low_temprature=42.0;
-ponj11.windspeed=0;
-ponj11.wind_direction="South East";
-ponj11.simple_information_to_display=["The party are hired by Mar to clear out her warehouse of rats.",
+Ponj11.high_temprature=48.0;
+Ponj11.low_temprature=42.0;
+Ponj11.windspeed=0;
+Ponj11.wind_direction="South East";
+Ponj11.simple_information_to_display=["The party are hired by Mar to clear out her warehouse of rats.",
     "Later the night the party realizes that Beece is missing.",
 "Darinthrall Shann reports alarming amounts of crow activity.",
 "Sherriff Raegalaine gets reports of three separate incidents where people were attacked by crows.",
     "The victims are sent to the Temple of Ehlonna under Halvards care."];
 
-ponj12.high_temprature=41.0;
-ponj12.low_temprature=32.0;
-ponj12.windspeed=0;
-ponj12.simple_information_to_display=["Party asks around after Beece, check with his family.  " +
+Ponj12.high_temprature=41.0;
+Ponj12.low_temprature=32.0;
+Ponj12.windspeed=0;
+Ponj12.simple_information_to_display=["Party asks around after Beece, check with his family.  " +
 "They say he used to be a painter before the Elsir War, and when they saved up enough money he had bought a studio to work from. " +
 "They also say that for the last two weeks he has not been himself, barely sleeping, and complaining of nightmares.",
     "A swarm of dessicated crows attack the party.",
@@ -183,11 +183,11 @@ ponj12.simple_information_to_display=["Party asks around after Beece, check with
     "Their skin has become covered in tiny black feathers and small scaly patches of skin, suppurating fluid.","They recommend isolating the sick people, and request that Halvard cast 'Speak With Dead' on Beece tomorrow."];
 
 
-ponj13.high_temprature=43.0;
-ponj13.low_temprature=32.0;
-ponj13.windspeed=0;
-ponj13.wind_direction="None";
-ponj13.simple_information_to_display=["Seymour, Kelvin, and Bazz have contracted the disease.",
+Ponj13.high_temprature=43.0;
+Ponj13.low_temprature=32.0;
+Ponj13.windspeed=0;
+Ponj13.wind_direction="None";
+Ponj13.simple_information_to_display=["Seymour, Kelvin, and Bazz have contracted the disease.",
     "The party sets off into the woods, following Beece's Trail from the previous day.",
     "They find a mysterious unnatural fog enshrouded area of the forest, all the trees in and near it appear to be dead and partially rotted..",
     "While moving towards it they are ambushed by cultists in spooky robes and crow masks.",
@@ -200,15 +200,15 @@ ponj13.simple_information_to_display=["Seymour, Kelvin, and Bazz have contracted
     "They conclude that the tree he spoke of must be beyond that unnatural fog bank."
 ];
 
-ponj14.high_temprature=41.5;
-ponj14.low_temprature=23.0;
-ponj14.windspeed=17;
-ponj14.precipitation=8;
-ponj14.precipitation_type="Freezing Rain";
-ponj14.precipitation_duration=8;
-ponj14.is_a_storm=true;
-ponj14.wind_direction="South";
-ponj14.simple_information_to_display=["The party gets ready for their journey into the fog.",
+Ponj14.high_temprature=41.5;
+Ponj14.low_temprature=23.0;
+Ponj14.windspeed=17;
+Ponj14.precipitation=8;
+Ponj14.precipitation_type="Freezing Rain";
+Ponj14.precipitation_duration=8;
+Ponj14.is_a_storm=true;
+Ponj14.wind_direction="South";
+Ponj14.simple_information_to_display=["The party gets ready for their journey into the fog.",
     "They visit Lady Amelia NeMoren in NeMoren Manor and question her about the original curse on the town.",
     "Lizettes two brothers kidnapped her (Lady NeMoren) and brought her to the Icecaves on the road west of here where Lizette imprisoned her in ice.",
     "They head into the woods after this and return to the strange fog.",
@@ -238,34 +238,98 @@ ponj14.simple_information_to_display=["The party gets ready for their journey in
     "It then flies off with the enourmous swarm of crows chaing it.",
     "The sigil on the evil sword was the house Sigil of the Beln family, Lizettes famliy..."];
 
-ponj15.high_temprature=53.0;
-ponj15.low_temprature=38.0;
-ponj15.windspeed=17;
-ponj15.precipitation=7;
-ponj15.wind_direction="South West";
-ponj15.simple_information_to_display=["More to come"];
+Ponj15.high_temprature=53.0;
+Ponj15.low_temprature=38.0;
+Ponj15.windspeed=17;
+Ponj15.precipitation=7;
+Ponj15.wind_direction="South West";
+Ponj15.simple_information_to_display=["More to come","Tor Oakheart and Amaria Goodman join the party.","Kelvin goes on a mission with Aniel."];
 
-ponj16.high_temprature=40.0;
-ponj16.low_temprature=24.0;
-ponj16.windspeed=5;
-ponj16.wind_direction="South West";
-ponj16.simple_information_to_display=["More to come"];
+Ponj16.high_temprature=40.0;
+Ponj16.low_temprature=24.0;
+Ponj16.windspeed=5;
+Ponj16.wind_direction="South West";
+Ponj16.simple_information_to_display=["More to come"];
 
-ponj17.high_temprature=50.0;
-ponj17.low_temprature=36.0;
-ponj17.windspeed=9;
-ponj17.wind_direction="West";
-ponj17.simple_information_to_display=["The team prepares for the battle tomorrow."];
+Ponj17.high_temprature=50.0;
+Ponj17.low_temprature=36.0;
+Ponj17.windspeed=9;
+Ponj17.wind_direction="West";
+Ponj17.simple_information_to_display=["The team prepares for the battle tomorrow."];
 
-ponj18.high_temprature=41.0;
-ponj18.low_temprature=30.0;
-ponj18.windspeed=15;
-ponj18.precipitation=3;
-ponj18.precipitation_type="Snow";
-ponj18.precipitation_duration=6;
-ponj18.wind_direction="North West";
-ponj18.simple_information_to_display=["First Sleet Storm of Fall at 7pm.","Reports of growing numbers of crows in the fields from outlying farmers."];
+Ponj18.high_temprature=45.0;
+Ponj18.low_temprature=30.0;
+Ponj18.windspeed=15;
+Ponj18.precipitation=3;
+Ponj18.precipitation_type="Snow";
+Ponj18.precipitation_duration=6;
+Ponj18.wind_direction="North West";
+Ponj18.simple_information_to_display=[
+    "The North Gates Sabotage",
+    "The Battle for Weston.",
+    "The Snow Begins to Fall",
+    "Lizette is Slain",
+    "Sithril's Betrayal",
+    "The Flight From Weston"];
 
+Ponj19.high_temprature=33.0;
+Ponj19.low_temprature=22.0;
+Ponj19.windspeed=4;
+Ponj19.wind_direction="East";
+Ponj19.simple_information_to_display=["More to come"];
 
+Ponj20.high_temprature=41.0;
+Ponj20.low_temprature=23.0;
+Ponj20.windspeed=11;
+Ponj20.precipitation=2;
+Ponj20.precipitation_type="Light Snow";
+Ponj20.precipitation_duration=8;
+Ponj20.wind_direction="East";
+Ponj20.simple_information_to_display=["Kelvin leading the surviving guard encounters the train of captured civilians coming from the caves they were sent to.",
+    "He meets up with Sarte the Soletaken who takes them and the group of kids and young adults to the old Shann Encampment.",
+    "He moves north to Cedric of The Grove and the rest of his team.",
+    ];
+
+Ponj21.high_temprature=33.0;
+Ponj21.low_temprature=22.0;
+Ponj21.windspeed=3;
+Ponj21.wind_direction="East";
+Ponj21.simple_information_to_display=["The Warm Mesa and Cedric.","The Party heads for the Centaurs."];
+
+Ponj22.high_temprature=33.0;
+Ponj22.low_temprature=26.0;
+Ponj22.windspeed=20;
+Ponj22.wind_direction="South East";
+Ponj22.simple_information_to_display=[""];
+
+Ponj23.high_temprature=31.0;
+Ponj23.low_temprature=28.0;
+Ponj23.windspeed=5;
+Ponj23.wind_direction="South East";
+Ponj23.simple_information_to_display=[""];
+
+Ponj24.high_temprature=28.0;
+Ponj24.low_temprature=15.0;
+Ponj24.windspeed=3;
+Ponj24.wind_direction="East";
+Ponj24.simple_information_to_display=[""];
+
+Ponj25.high_temprature=24.0;
+Ponj25.low_temprature=14.0;
+Ponj25.windspeed=3;
+Ponj25.wind_direction="North East";
+Ponj25.simple_information_to_display=[""];
+
+Ponj26.high_temprature=22.0;
+Ponj26.low_temprature=(2.0-4.0);
+Ponj26.windspeed=3;
+Ponj26.wind_direction="North East";
+Ponj26.simple_information_to_display=[""];
+
+Ponj27.high_temprature=10.0;
+Ponj27.low_temprature=(0.0-8.0);
+Ponj27.windspeed=3;
+Ponj27.wind_direction="East";
+Ponj27.simple_information_to_display=[""];
 
 
