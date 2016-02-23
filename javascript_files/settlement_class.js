d3cc07c1050f18d06f018688224c2d61e909140b
ShivonQ <realitykeeper@gmail.com>
2016-02-22 21:59:34 -0600

    Map Page:    No Change Until File Hosting
    Calendar Page:
      Date information is now working perfectly. Utilized local storage since wit 2 windows the storages become different immediately.
    
      NEXT: Make a new Branch for getting the lists of event data to display for each day.
    
     Main Page:
    
     Date Class + Data
       I have added lots of information on Ponj.

---
+++ javascript_files/settlement_class.js
@@ -0,0 +1,11 @@
+/**
+ * Created by School on 2/22/2016.
+ */
+var list_of_settlement_types={1:"Thorp",2:"Hamlet",3:"Village",4:"Small Town",5:"Large Town",6:"Small City",7:"Large City",8:"Metropolis"}
+//since at the moment this class CANNOT take information since my whole world is already hardcoded I will just be doing the selection of capital cities and towns
+var Settlement_Data=function(){
+
+}
+
+
+
